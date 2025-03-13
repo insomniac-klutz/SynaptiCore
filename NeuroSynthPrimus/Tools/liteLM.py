98dbbc8af64d322cb402b39b29ee7eac9bf8d3a6
Sankalp Chourasia <42043829+insomniac-klutz@users.noreply.github.com>
2025-03-14 03:18:44 +0530

    add : liteLM base

---
+++ NeuroSynthPrimus/Tools/liteLM.py
@@ -0,0 +1,8 @@
+from langchain_community.chat_models import ChatLiteLLM
+from smolagents import LiteLLMModel
+
+
+#gemini_flash_2_exp_endpoints
+MODEL_GEMINI_FLASH_2_EXP = "gemini/gemini-2.0-flash-exp"
+lang_gemini_flash_2_exp = ChatLiteLLM(model_id=MODEL_GEMINI_FLASH_2_EXP)
+smol_gemini_flash_2_exp = LiteLLMModel(model=MODEL_GEMINI_FLASH_2_EXP)
